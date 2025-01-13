9752e78a5138cbd061d732c2d59672706addcf86
zhangjinbao <zhangjinbao0808@outlook.com>
2025-01-13 13:30:11 +0800

    feat: 新增Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/next',
+  output: "standalone",
 };
 
 export default nextConfig;
